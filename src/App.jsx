498e60caf4b07b7fecb7e64a277eb427924b10e9
baba-shashank <shashanknayak0702@gmail.com>
2024-10-15 21:33:19 +0530

    initial structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
-  return <h1>hello react</h1>;
+  return (
+    <center>
+      <h1>to do APP</h1>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-sm-4">
+            <input type="text" />
+          </div>
+          <div class="col-sm-4">
+            <input type="date" name="" id="" />
+          </div>
+          <div class="col-sm-2">
+            <button type="button" class="btn btn-success">
+              Success
+            </button>
+          </div>
+        </div>
+      </div>
+    </center>
+  );
 }
 
 export default App;
